7354f1e7268ee817234ca2591ec662d43ae4218e
NikhitaBoyidapu <nboyidap@uncc.edu>
2023-09-17 22:51:16 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const port=3000;
 
 app.use('/',express.static('public'));
 
-app.get('/budget',(req,res) => {
-    res.json(budget);
+const budget = {
+    myBudget: [
+{
+    title: 'Eat out',
+    budget: 25
+},
+{
+    title: 'Rent',
+    budget: 375
+}
+]};
+
+app.use('/budget',(req,res) => {
+    res.sendFile('C:\\dev\\personal-budget1\\quiz4.json');
 });
 
 app.get('/hello',(req,res) => {
     res.send('Hello World!');
 });
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
